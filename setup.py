860e1ee42993d2524eeefb0fd63a883b2ced9718
Adam Parkin <aparkin@seekersolutions.com>
2012-12-23 20:30:19 -0800

    Added CHANGES.txt & cleaned up setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ setup(
     version='1.0',
     author="Adam Parkin",
     author_email="pzelnip@gmail.com",
+    url="https://github.com/pzelnip/XboxLeadersAPI",
+    description="A simple Python wrapper for the XboxLeaders.com Xbox API (http://www.xboxleaders.com/docs/api)",
     packages=['xboxleaders',],
     license='FreeBSD',
     long_description=open('README').read(),
