6cb802d464b4cd1cdaf91f441b6f538672c352e8
Adam Parkin <aparkin@seekersolutions.com>
2012-12-23 20:26:07 -0800

    Fixed a few things

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@ from distutils.core import setup
 setup(
     name='XboxLeadersAPI',
     version='1.0',
+    author="Adam Parkin",
+    author_email="pzelnip@gmail.com",
     packages=['xboxleaders',],
     license='FreeBSD',
-    long_description=open('README.md').read(),
+    long_description=open('README').read(),
 )
