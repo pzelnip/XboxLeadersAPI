97ff0188f4b1fafe6aa9c9738813557bba0ce4c8
Adam Parkin <aparkin@seekersolutions.com>
2012-12-23 20:53:30 -0800

    Changed __init__ to import module functions & updated setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,12 @@ setup(
     url="https://github.com/pzelnip/XboxLeadersAPI",
     description="A simple Python wrapper for the XboxLeaders.com Xbox API (http://www.xboxleaders.com/docs/api)",
     packages=['xboxleaders',],
-    license='FreeBSD',
+    license='BSD',
     long_description=open('README').read(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords=['xbox xbox360 xboxleaders'],
 )
--- xboxleaders/__init__.py
+++ xboxleaders/__init__.py
@@ -1,3 +1,6 @@
 '''
 A simple Python wrapper for the XboxLeaders.com Xbox API By Jason Clemons
 '''
+
+from .xboxleaders import fetch_profile, fetch_games, fetch_achievements, \
+    fetch_friends
